33a3761222caec4694afb059902791c30259be39
Michele Ye <michele.ye.dev@gmail.com>
2025-07-23 20:41:32 -0300

    correção da formatação

--- node-scraper/produto_scraper.js
+++ node-scraper/produto_scraper.js
@@ -10,28 +10,27 @@ class Produto{
         this.imagem = null;
     }
     async extrair_dados() {
-        const response = await axios.get(this.url);
-        const html = response.data;
-        const $ = cheerio.load(html);
-        this.titulo = $('.showcase__description > h1').text().trim();
-        this.descricao = $('.feature__main-content').text().trim();
-        this.preco = $('.saleInCents-value').first().text().trim();
-        this.imagem = $('.image-presenter__carousel img').attr('src');
+        try{
+            const response = await axios.get(this.url);
+            const html = response.data;
+            const $ = cheerio.load(html);
+            this.titulo = $('.showcase__description > h1').text().trim();
+            this.descricao = $('.feature__main-content').text().trim();
+            this.preco = $('.saleInCents-value').first().text().trim();
+            this.imagem = $('.image-presenter__carousel img').attr('src');
+        }
+        catch (erro) {
+            console.error("Erro:", erro.message);
+        }
     }
 }
 
-const url = 'https://www.netshoes.com.br/p/mochila-nike-heritage-preto+branco-2IC-3604-026';
-async function main() {
-  const produto = new Produto(url);
-  await produto.extrair_dados();  
-  console.log(produto.titulo);  
-  console.log(produto.descricao);
-  console.log(produto.preco);
-  console.log(produto.imagem);
-}
-
-main();
-
-// const produto = new Produto(url)
-// produto.extrair_dados()
-// console.log(produto.titulo)
+(async() => {
+    const url = 'https://www.netshoes.com.br/p/mochila-nike-heritage-preto+branco-2IC-3604-026';
+    const produto = new Produto(url);
+    await produto.extrair_dados();  
+    console.log(`Título do produto: ${produto.titulo}\n`);  
+    console.log(`Descrição: ${produto.descricao}\n`);
+    console.log(`Preço: ${produto.preco}\n`);
+    console.log(`Url da imagem: ${produto.imagem}`);
+})()
